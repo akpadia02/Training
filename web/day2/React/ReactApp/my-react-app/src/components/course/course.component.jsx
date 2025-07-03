d921a5663b7f8711dec56061e0a3f7e75d930b91
akpadia02 <akpadia02@gmail.com>
2025-07-03 12:42:09 +0530

    state and setState on Likes

--- web/day2/React/ReactApp/my-react-app/src/components/course/course.component.jsx
+++ web/day2/React/ReactApp/my-react-app/src/components/course/course.component.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 export default class Course extends Component {
+  state={curLikes:this.props.coursedetails.likes};
   // renderStars = () => {
   //   const rating = this.props.coursedetails.rating;
   //   const stars = [];
@@ -12,6 +13,12 @@ export default class Course extends Component {
   //   return stars;
   // };
 
+  incrementLikes(){
+    console.log("clicked");
+    // this.props.coursedetails.likes++;// props are readonly (ie need states to reflect ui)
+    //this.state.curLikes++; //states are immutable
+    this.setState({curLikes:this.state.curLikes+1});
+  }
   renderStars = () => {
     const rating = this.props.coursedetails.rating;
     const fullStars = Math.floor(rating);
@@ -52,17 +59,17 @@ export default class Course extends Component {
 
     return stars;
   };
-  
+
   render() {
     return (
         <div className="col-md-3 my-1">
       <div className="card">
-        <img src={this.props.coursedetails.imageUrl} class="card-img-top" alt="..."  style={{height: "200px"}}/>
-        <div class="card-body">
+        <img src={this.props.coursedetails.imageUrl} className="card-img-top" alt="..."  style={{height: "200px"}}/>
+        <div className="card-body">
             <h1>{this.props.coursedetails.title}</h1>
-            <p> <i class="fa-solid fa-indian-rupee-sign"></i> {this.props.coursedetails.price}</p>
+            <p> <i className="fa-solid fa-indian-rupee-sign"></i> {this.props.coursedetails.price}</p>
             <p>Rating: {this.renderStars()} </p>
-            <button class="btn btn-outline-primary"><i class="fa-solid fa-thumbs-up"></i> {this.props.coursedetails.likes}</button>
+            <button className="btn btn-outline-primary" onClick={()=>this.incrementLikes()}><i className="fa-solid fa-thumbs-up"></i> {this.state.curLikes}</button>
         </div>
       </div>
       </div>
