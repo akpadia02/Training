460104c61bd0049eb6235747a6396298772cbaed
akpadia02 <akpadia02@gmail.com>
2025-07-04 16:49:46 +0530

    post delete and get in express

--- web/day4/NodeJs/ExpressBasic/src/server/app.js
+++ web/day4/NodeJs/ExpressBasic/src/server/app.js
@@ -1,6 +1,6 @@
 // import express from "express";
-const express =require("express");
-const path=require("path");
+const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000
 
@@ -21,18 +21,41 @@ const port = 3000
 
 
 app.use(express.static('../client'));
-app.use(express.static(path.join(__dirname, '../client')));
- //middleware->extra functionality on top of default function
+// app.use(express.static(path.join(__dirname, '../client')));
+//middleware->extra functionality on top of default function
+
+
+//get post delete
+app.use(express.json()); //express.json to parse json req body
+
+var products = [{ id: 1, name: "TV", price: 4000 },
+{ id: 2, name: "Laptop", price: 30000 },
+{ id: 3, name: "Bottle", price: 4040 },
+{ id: 4, name: "Mobile", price: 40500 },
+{ id: 5, name: "PC", price: 40090 },
+];
+
+
+app.post('/newProduct', (req, res) => {
+    const payload=req.body;
+    console.log(payload);
+    products.push(payload);
+    res.send("Product added successfully!");
+})
+
+app.delete('/deleteProduct/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = products.findIndex(p => p.id === id);
+    if (index !== -1) {
+        products.splice(index, 1);
+        res.send(`Product with id ${id} deleted.`);
+    } else {
+        res.status(404).send(`Product with id ${id} not found.`);
+    }
+});
 
 
 app.get('/products', (req, res) => {
-
-    var products = [{ id: 1, name: "TV", price: 4000 },
-    { id: 2, name: "Laptop", price: 30000 },
-    { id: 3, name: "Bottle", price: 4040 },
-    { id: 4, name: "Mobile", price: 40500 },
-    { id: 5, name: "PC", price: 40090 },
-    ];
     res.send(products)
 })
 
